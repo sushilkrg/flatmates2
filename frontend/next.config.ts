94c6443362f783f5379dc673a8358058b8b1a65a
Sushil Kumar <sushilkr820@gmail.com>
2025-11-30 14:29:04 +0530

    removes rewrites

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,15 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        destination: "https://flatmates-api.onrender.com/api/v1/:path*", 
-        // Replace with your actual backend URL (Render/Railway/EC2)
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/v1/:path*",
+  //       destination: "https://flatmates-api.onrender.com/api/v1/:path*", 
+  //       // Replace with your actual backend URL (Render/Railway/EC2)
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
