4d952131fd314e945a8e036dd76cd5464a37f008
Sushil Kumar <sushilkr820@gmail.com>
2025-12-01 11:48:19 +0530

    removed domain

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -17,7 +17,6 @@ export const generateTokenAndSetCookies = (userId: any, res: Response) => {
     //  local server
     // secure: true, // must be false on localhost
     // sameSite: "lax", // safe default - lax on localhost
-    domain: ".vercel.app",
     secure: true,
     sameSite: "none",
 
